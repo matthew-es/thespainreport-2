87b3d9b3bfdac7d6d8765d5e8bd19a4a091a3ac7
Matthew Bennett <matthew@thespainreport.com>
2019-01-21 16:25:46 +0000

    Head tags and titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,27 +1,35 @@
 module ApplicationHelper
+	
+	def title(page_title)
+    content_for :title, page_title.to_s
+	end
+	
+	def meta
+    content_for :meta
+	end
 
-  def markdown(text)
-    options = {
-      filter_html: true,
-      hard_wrap: true,
-      space_after_headers: true, 
-      fenced_code_blocks: true,
-      with_toc_data: true
-    }
+	def markdown(text)
+		options = {
+			filter_html: true,
+			hard_wrap: true,
+			space_after_headers: true, 
+			fenced_code_blocks: true,
+			with_toc_data: true
+		}
 
-    extensions = {
-      no_intra_emphasis: true,
-      tables: true,
-      strikethrough: true,
-      superscript: true,
-      disable_indented_code_blocks: true,
-      highlight: true
-    }
+		extensions = {
+			no_intra_emphasis: true,
+			tables: true,
+			strikethrough: true,
+			superscript: true,
+			disable_indented_code_blocks: true,
+			highlight: true
+		}
 
-    renderer = Redcarpet::Render::HTML.new(options)
-    markdown = Redcarpet::Markdown.new(renderer, extensions)
+		renderer = Redcarpet::Render::HTML.new(options)
+		markdown = Redcarpet::Markdown.new(renderer, extensions)
 
-    markdown.render(text).html_safe
-  end
+		markdown.render(text).html_safe
+	end
 
 end
