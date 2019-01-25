2c696ad918118c652cb3598caf0f2d21158c24c5
Matthew Bennett <matthew@thespainreport.com>
2019-01-25 19:12:14 +0000

    Lists component articles on story, podcast pages

--- app/models/type.rb
+++ app/models/type.rb
@@ -2,7 +2,7 @@ class Type < ApplicationRecord
     belongs_to :language
     has_many :articles
     
-    scope :story, -> {where(name: ['Story', 'Podcast'])}
+    scope :story, -> {where(name: ['Story', 'Podcast', 'Tema'])}
     scope :notstory, -> {where.not(name: 'Story')}
     scope :notes, -> {where(name: ['Notes', 'Apuntes'])}
     scope :notnotes, -> {where.not(name: ['Notes', 'Apuntes'])}
