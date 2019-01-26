befedaa9fff370a9206f0307f012f1dd6869e173
Matthew Bennett <matthew@thespainreport.com>
2019-01-26 14:35:30 +0000

    Adds basic RSS feed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,11 +3,21 @@ class HomeController < ApplicationController
     @lasttopstory = Article.notnotes.english.topstory.published.lastone
     @lastten = Article.notlatesttop.notnotes.english.published.lastten
     @lasttennotes = Article.notes.english.published.lastten
+    @rss = Article.all.order('created_at DESC')
+    respond_to do |format|
+      format.html
+      format.rss { render :layout => false }
+    end
   end
   
   def es
     @lasttopstory = Article.notnotes.spanish.topstory.published.lastone
     @lastten = Article.notlatesttop.notnotes.spanish.published.lastten
     @lasttennotes = Article.notes.spanish.published.lastten
+    @rss = Article.all.spanish.order('created_at DESC')
+    respond_to do |format|
+      format.html
+      format.rss { render :layout => false }
+    end
   end
 end
---
+++ app/views/home/index.rss.builder
@@ -0,0 +1,18 @@
+xml.instruct! :xml, :version => "1.0"
+xml.rss :version => "2.0" do
+  xml.channel do
+    xml.title "The Spain Report"
+    xml.description "Indpendent reportings and analysis of news from Spain"
+    xml.link root_url
+
+    @rss.each do |article|
+      xml.item do
+        xml.title article.headline
+        xml.description article.lede
+        xml.pubDate article.created_at.to_s(:rfc822)
+        xml.link article_url(article)
+        xml.guid article_url(article)
+      end
+    end
+  end
+end
