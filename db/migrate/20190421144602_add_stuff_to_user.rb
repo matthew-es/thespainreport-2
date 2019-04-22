682ead71c6e7c6a7f30d086f04f21993af710a27
Matthew Bennett <matthew@thespainreport.com>
2019-04-22 19:47:59 +0000

    Attempt 1, fix postgres migration problem

--- db/migrate/20190421144602_add_stuff_to_user.rb
+++ db/migrate/20190421144602_add_stuff_to_user.rb
@@ -1,6 +1,6 @@
 class AddStuffToUser < ActiveRecord::Migration[5.2]
   def change
-    change_column :users, :role, :integer
+    change_column :users, :role, 'integer USING CAST(role AS integer)'
     add_column :users, :emails, :integer
     add_column :users, :emaillanguage, :integer
     add_column :users, :sitelanguage, :integer
