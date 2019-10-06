e77480a054302d471de3680a111d1323b46de509
Matthew Bennett <matthew@thespainreport.com>
2019-10-06 14:08:34 +0000

    Temporary routes redirection for payments

--- config/routes.rb
+++ config/routes.rb
@@ -16,17 +16,27 @@ Rails.application.routes.draw do
 
   root 'home#index'
   
-  # get 'contribute', to: redirect("/")
+  
+  get 'guarantee', to: redirect("/")
+  get 'garantizar', to: redirect("/")
+  get 'support', to: redirect("/")
+  get 'apoyar', to: redirect("/")
+  get 'contribute', to: redirect("/")
+  get 'contribuir', to: redirect("/")
+  get 'pay', to: redirect("/")
+  get 'pagar', to: redirect("/")
+  
+  # get 'guarantee' => "payments#pay"
+  # get 'garantizar' => "payments#pagar"
+  # get 'support' => "payments#pay"
+  # get 'apoyar' => "payments#pagar"
+  # get 'contribute' => "payments#pay"
+  # get 'contribuir' => "payments#pagar"
+  # get 'pay' => "payments#pay"
+  # get 'pagar' => "payments#pagar"
+  
   get 'trial', to: redirect("http://www.thespainreport.es/articles/32-180101210225-catalan-separatism")
   get 'twitterpatreon' => 'home#patreon'
-  get 'guarantee' => "payments#pay"
-  get 'garantizar' => "payments#pagar"
-  get 'support' => "payments#pay"
-  get 'apoyar' => "payments#pagar"
-  get 'contribute' => "payments#pay"
-  get 'contribuir' => "payments#pagar"
-  get 'pay' => "payments#pay"
-  get 'pagar' => "payments#pagar"
   post 'stripe_first_payment', to: redirect("/")
   get 'confirm', to: redirect("/")
   post 'stripe_webhook' => 'payments#stripe_webhook'
