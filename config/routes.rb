a17860eb7267e828230bde9f7178ecdc35960713
Matthew Bennett <matthew@thespainreport.com>
2019-12-02 12:28:54 +0000

    Shuts off routes, payment elements

--- config/routes.rb
+++ config/routes.rb
@@ -17,43 +17,43 @@ Rails.application.routes.draw do
 
   root 'home#index'
   
-  resources :payments
-  # get 'guarantee', to: redirect("/")
-  # get 'garantizar', to: redirect("/")
-  # get 'support', to: redirect("/")
-  # get 'apoyar', to: redirect("/")
-  # get 'contribute', to: redirect("/")
-  # get 'contribuir', to: redirect("/")
-  # get 'pay', to: redirect("/")
-  # get 'pagar', to: redirect("/")
+  # resources :payments
+  get 'guarantee', to: redirect("/")
+  get 'garantizar', to: redirect("/")
+  get 'support', to: redirect("/")
+  get 'apoyar', to: redirect("/")
+  get 'contribute', to: redirect("/")
+  get 'contribuir', to: redirect("/")
+  get 'pay', to: redirect("/")
+  get 'pagar', to: redirect("/")
   
-  get 'pay', to: redirect('subscribe/guarantee')
-  get 'pay/:slug', to: redirect { |path, req| "subscribe/#{path[:slug]}" }
-  get 'contribute', to: redirect('subscribe/contribute')
-  get 'contribute/:slug', to: redirect { |path, req| "subscribe/#{path[:slug]}" }
-  get 'guarantee', to: redirect('subscribe/guarantee')
-  get 'guarantee/:slug', to: redirect { |path, req| "subscribe/#{path[:slug]}" }
-  get 'support', to: redirect('subscribe/support')
-  get 'support/:slug', to: redirect { |path, req| "subscribe/#{path[:slug]}" }
-  get 'subscribe', to: redirect('subscribe/subcribe')
-  get 'subscribe/:slug' => "payments#pay"
+  # get 'pay', to: redirect('subscribe/guarantee')
+  # get 'pay/:slug', to: redirect { |path, req| "subscribe/#{path[:slug]}" }
+  # get 'contribute', to: redirect('subscribe/contribute')
+  # get 'contribute/:slug', to: redirect { |path, req| "subscribe/#{path[:slug]}" }
+  # get 'guarantee', to: redirect('subscribe/guarantee')
+  # get 'guarantee/:slug', to: redirect { |path, req| "subscribe/#{path[:slug]}" }
+  # get 'support', to: redirect('subscribe/support')
+  # get 'support/:slug', to: redirect { |path, req| "subscribe/#{path[:slug]}" }
+  # get 'subscribe', to: redirect('subscribe/subcribe')
+  # get 'subscribe/:slug' => "payments#pay"
   
-  get 'suscribirse/' => "payments#pagar"
-  get 'suscribirse/:slug' => "payments#pagar"
-  get 'garantizar' => "payments#pagar"
-  get 'apoyar' => "payments#pagar"
-  get 'contribuir' => "payments#pagar"
-  get 'pagar' => "payments#pagar"
+  # get 'suscribirse/' => "payments#pagar"
+  # get 'suscribirse/:slug' => "payments#pagar"
+  # get 'garantizar' => "payments#pagar"
+  # get 'apoyar' => "payments#pagar"
+  # get 'contribuir' => "payments#pagar"
+  # get 'pagar' => "payments#pagar"
   
-  get 'stripe_get_payment_intent' => 'payments#stripe_get_payment_intent'
-  post 'check_if_user' => 'payments#check_if_user'
-  post 'new_payment_error' => 'payment_errors#create_payment_error'
-  post 'stripe_credit_card' => 'payments#stripe_credit_card'
-  post 'stripe_first_payment' => 'payments#stripe_first_payment'
-  get 'confirm_credit_card' => 'payments#stripe_confirm_payment'
-  get 'confirmar_tarjeta' => 'payments#stripe_confirm_payment'
+  # get 'stripe_get_payment_intent' => 'payments#stripe_get_payment_intent'
+  # post 'check_if_user' => 'payments#check_if_user'
+  # post 'new_payment_error' => 'payment_errors#create_payment_error'
+  # post 'stripe_credit_card' => 'payments#stripe_credit_card'
+  # post 'stripe_first_payment' => 'payments#stripe_first_payment'
+  # get 'confirm_credit_card' => 'payments#stripe_confirm_payment'
+  # get 'confirmar_tarjeta' => 'payments#stripe_confirm_payment'
   
-  post '/webhook_events/:source', to: 'webhook_events#create'
+  # post '/webhook_events/:source', to: 'webhook_events#create'
   get 'trial', to: redirect("http://www.thespainreport.es/articles/32-180101210225-catalan-separatism")
   get 'twitterpatreon' => 'home#patreon'
   
