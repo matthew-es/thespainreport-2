82d21a9b8d2be5af89edabefd46cfb1889252ba4
Matthew Bennett <matthew@thespainreport.com>
2020-02-17 12:25:28 +0000

    More for patrons, article emails, better code in mailer

--- app/mailers/article_mailer.rb
+++ app/mailers/article_mailer.rb
@@ -5,10 +5,19 @@ class ArticleMailer < ApplicationMailer
 	
 	def send_article_full new_mail, user
 		@article = new_mail
-		@user = user
 		@articletweets = @article.tweets.order('created_at ASC')
 		@articleupdates = @article.updates.published.order('created_at ASC')
+		@language = @article.language_id
+		if @language == 1
+			@value = "value/"
+		elsif @language == 2
+			@value = "valor/"
+		else end
+		@subscribe = root_url + @value + @article.frame.link_slug
 		
+		@user = user
+		@level = @user.level_amount
+			
 		headers 'X-SES-CONFIGURATION-SET' => "Emails"
 		mail(:to => "<#{@user.email}>", :subject => email_subject)
 	end
