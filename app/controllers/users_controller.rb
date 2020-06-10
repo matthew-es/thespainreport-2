1856ce44b5ba67ada691ec1c1da3de318edc6084
Matthew Bennett <matthew@thespainreport.com>
2020-06-10 16:25:51 +0000

    Comments out update level_amount in user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -325,18 +325,18 @@ class UsersController < ApplicationController
 			@latest_patrons_cancelled  = User.patrons_cancelled.limit(20).order("updated_at DESC")
 			@latest_authors = User.authors.order("updated_at DESC")
 			
-			User.all.each do |u|
-				if [1, 2].include?u.status
-					u.update(
-						level_amount: 1
-						)
-				elsif u.status == 3
-					u.update(
-						level_amount: 0
-						)
-				else
-				end
-			end
+#			User.all.each do |u|
+#				if [1, 2].include?u.status
+#					u.update(
+#						level_amount: 1
+#						)
+#				elsif u.status == 3
+#					u.update(
+#						level_amount: 0
+#						)
+#				else
+#				end
+#			end
 		else
 			redirect_to root_url
 		end
