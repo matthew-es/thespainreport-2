ce643f82f85df63abdd284e581a287c01643cd2a
Matthew Bennett <matthew@thespainreport.com>
2021-01-16 18:16:14 +0000

    Updates email logic for new article formats

--- app/mailers/article_mailer.rb
+++ app/mailers/article_mailer.rb
@@ -6,6 +6,7 @@ class ArticleMailer < ApplicationMailer
 	def send_article_full new_mail, user
 		@article = new_mail
 		@articletweets = @article.tweets.order('created_at ASC')
+		@articlepieces = @article.pieces.published.order('created_at ASC')
 		@language = @article.language_id
 		@frame = @article.frame
 		
@@ -24,17 +25,30 @@ class ArticleMailer < ApplicationMailer
 		
 		@user = user
 		@status = @user.status
-		@level = @user.level_amount
 		@can_read_date = @user.can_read_date
-		@account_status = @user.account.account_status
-			
+		@level = @user.level_amount
+		@account_status = @user.account.account_status unless @user.account.nil?		
+		
+		@admin = @status == 1
+		@super_patron = @status == 2 && (@level > 25)
+		@patron_reader_25 = @status == 2 && (@level == 25)
+		@patron_reader_10 = @status == 2 && @level.between?(10, 24)
+		@patron_reader_5 = @status == 2 && @level.between?(5, 9)
+		@patron_reader_1 = @status == 2 && @level.between?(1, 4)
+		@patron_reader_0 = @status == 2 && (@level == 0)
+		@patron = @patron_reader_0 || @patron_reader_1 || @patron_reader_5 || @patron_reader_10 || @patron_reader_25
+		@reader_trial = @status == 3 && @can_read_date > Time.now
+		@reader_trial_over = @status == 3 && @can_read_date < Time.now
+		
+		@can_read_level_1 = @admin || @reader_trial|| @super_patron || @patron_reader_25 || @patron_reader_10 || @patron_reader_5  || @patron_reader_1
+		
 		headers 'X-SES-CONFIGURATION-SET' => "Emails"
 		mail(:to => "<#{@user.email}>", :subject => email_subject)
 	end
 	
 	def email_subject
 		case @article.type.name
-			when "Patrons only", "Sólo mecenas" then @emoji = "🔓 🔥 💎 📝"
+			when "Patrons only", "Sólo mecenas" then @emoji = "🔓 📝"
 			when "Video", "Vídeo" then @emoji = "🎥"
 			when "Podcast" then @emoji = "🎧"
 			when "Photos", "Fotos" then @emoji = "📷"
