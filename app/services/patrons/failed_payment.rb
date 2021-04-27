2f448d609354d5225c0dd7541a6f6c4f12056ec0
Matthew Bennett <matthew@thespainreport.com>
2021-04-27 17:15:22 +0000

    Checks 0 VAT on repeat payments, adds admin emails on payment success/problem

--- app/services/patrons/failed_payment.rb
+++ app/services/patrons/failed_payment.rb
@@ -4,7 +4,10 @@ module Patrons
         	
         	payment.update(status: "problem")
         	PaymentMailer.payment_problem(payment).deliver_now
-
+        	
+        	admin_subject = ("\u274C").force_encoding('utf-8') + " PROBLEM: " + payment.payment_type.upcase + " " + (ActiveSupport::NumberHelper.number_to_currency((payment.total_amount/100.to_f), unit: "€")).to_s + " from " + payment.account.user.email
+			admin_message = ""
+			PaymentMailer.payment_admin_message(admin_subject, admin_message).deliver_now
         end
     end
 end
--- app/services/patrons/successful_payment.rb
+++ app/services/patrons/successful_payment.rb
@@ -9,6 +9,10 @@ module Patrons
 
 			Patrons::CreateInvoice.process(payment) unless Invoice.where(payment_id: payment.id, account_id: payment.account.id, invoice_operation: "payment").count > 0
 			PaymentMailer.payment_success(payment).deliver_now unless payment.updated_at < 1.seconds.ago(Time.now)
+			
+			admin_subject = ("\u2705").force_encoding('utf-8') + " " + payment.payment_type.upcase + " " + (ActiveSupport::NumberHelper.number_to_currency((payment.total_amount/100.to_f), unit: "€")).to_s + " from " + payment.account.user.email
+			admin_message = ""
+			PaymentMailer.payment_admin_message(admin_subject, admin_message).deliver_now
         end
     end
 end
